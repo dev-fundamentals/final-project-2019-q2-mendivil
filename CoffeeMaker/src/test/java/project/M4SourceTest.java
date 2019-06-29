d070a5a397e7faa58d4ad2d338cc5fdebfbe3ec8
Lizzy Mendivil <lizzy.mendivil@jalasoft.com>
2019-06-29 16:21:04 -0400

    Add M4Source and M4Vessel classes for Unit Test

---
+++ CoffeeMaker/src/test/java/project/M4SourceTest.java
@@ -0,0 +1,14 @@
+package test.java.project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class M4SourceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
